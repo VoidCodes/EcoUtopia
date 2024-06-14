d8fb3937da01e29402a665c9e1046ad488ec7e05
bengyman <131242394+bengyman@users.noreply.github.com>
2024-06-14 14:59:43 +0800

    Create post.js

---
+++ server/models/post.js
@@ -0,0 +1,49 @@
+// post.model.js
+module.exports = (sequelize, DataTypes) => {
+    const Post = sequelize.define('Post', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        tags: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            field: 'user_id', // Ensure this matches the actual field name in the database
+            references: {
+                model: 'users', // Adjust based on the actual table name in your database
+                key: 'user_id' // Adjust based on the actual column name in your database
+            }
+        },
+        imageUrl: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        reports: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+        }
+    }, {
+        tableName: 'posts',
+        timestamps: true, // Adjust based on your requirements
+        createdAt: 'createdAt', // Ensure these match the actual column names in your database
+        updatedAt: 'updatedAt'
+    });
+
+    Post.associate = function(models) {
+        Post.belongsTo(models.users, { // Adjust based on the actual table name in your database
+            foreignKey: 'userId',
+            as: 'user'
+        });
+    };
+
+    return Post;
+};
