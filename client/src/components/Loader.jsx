4b3ae61924adb67d73bc3da71b14e43df2f88c32
VoidCodes <234273T@mymail.nyp.edu.sg>
2024-07-05 10:53:12 +0800

    feat: Add Loader component for better user experience

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,17 @@
+import { Loader } from '@mantine/core';
+
+const LoaderComponent = () => {
+  return (
+    <Loader
+      size={50}
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    />
+  );
+};
+
+export default LoaderComponent;
