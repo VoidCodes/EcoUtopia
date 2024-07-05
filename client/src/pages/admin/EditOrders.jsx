eae9da5aafab0a03b333f3776ec2d9a68558f308
VoidCodes <234273T@mymail.nyp.edu.sg>
2024-07-05 10:52:37 +0800

    feat: Add lazy loading for course instructor avatar in Courses page

--- client/src/pages/admin/EditOrders.jsx
+++ client/src/pages/admin/EditOrders.jsx
@@ -1,13 +1,33 @@
+import https from '../../https'
+import dayjs from 'dayjs'
+import global from '../../global'
+import LoaderComponent from '../../components/Loader'
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import https from '../../https';
-import dayjs from 'dayjs';
-import global from '../../global';
-import { Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import {
+  Container,
+  Card,
+  Text,
+  Button,
+  Modal,
+  Group,
+} from '@mantine/core';
+import { TiArrowBack } from "react-icons/ti";
 
 function EditOrders() {
   const { orderId } = useParams();
+  const navigate = useNavigate();
   const [order, setOrder] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 300); // Display loader for at least 0.3 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
 
   useEffect(() => {
     if (orderId) {
@@ -20,46 +40,80 @@ function EditOrders() {
   const handleRefund = () => {
     https.put(`/orders/${orderId}`).then((res) => {
       setOrder(res.data);
+      setIsModalOpen(false); // Close the modal
+      navigate('/orders'); // Navigate back to the orders page
     });
   };
 
-  if (!order) {
-    return <div>Loading...</div>;
+  if (isLoading || !order) {
+    return <LoaderComponent />;
   }
 
   return (
-    <div className="edit-order-container">
-      <h1>Edit Order {order.order_id}</h1>
-      <div className="order-details">
-        <p>
-          <strong>Course Title:</strong> {order.Course.course_name}
-        </p>
-        <p>
-          <strong>Order Status:</strong> {order.order_status}
-        </p>
-        <p>
-          <strong>Order Date:</strong> {dayjs(order.order_date).format(global.datetimeFormat)}
-        </p>
-        <p>
-          <strong>Instructor:</strong> {order.Course.course_instructor}
-        </p>
-        <p>
-          <strong>Course Description</strong> {order.Course.course_description}
-        </p>
-        <p>
-          <strong>Course Date:</strong> {dayjs(order.Course.course_date).format(global.datetimeFormat)}
-        </p>
-        <p>
-          <strong>Start Time:</strong> {order.Course.course_start_time}
-        </p>
-        <p>
-          <strong>End Time:</strong> {order.Course.course_end_time}
-        </p>
-        <Link to="/orders">
-        <button onClick={handleRefund}>Refund Order</button>
-        </Link>
-      </div>
-    </div>
+    <Container size="md" style={{ marginTop: 20 }}>
+      <Group position="apart" mb={30}>
+        <Button component={Link} to="/orders" variant="link">
+          <TiArrowBack />
+        </Button>
+        <Text
+          align="start"
+          weight={700}
+          style={{ fontSize: 30 }}
+          c="deepBlue"
+          fw={500}
+          size="xl"
+        >
+          Refund Order {order.order_id}
+        </Text>
+      </Group>
+
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Text size="lg" fw={700}>
+          Course Title: <Text component='span'>{order.Course.course_name}</Text>
+        </Text>
+        <Text size="lg" fw={700}>
+          Order Status: <Text component='span'>{order.order_status}</Text>
+        </Text>
+        <Text size="lg" fw={700}>
+          Order Date: <Text component='span'>{dayjs(order.order_date).format(global.datetimeFormat)}</Text>
+        </Text>
+        <Text size="lg" fw={700}>
+          Instructor: <Text component='span'>{order.Course.course_instructor}</Text>
+        </Text>
+        <Text size="lg" fw={700}>
+          Course Description: <Text component='span'>{order.Course.course_description}</Text>
+        </Text>
+        <Text size="lg" fw={700}>
+          Course Date: <Text component='span'>{dayjs(order.Course.course_date).format(global.dateFormat)}</Text>
+        </Text>
+        <Text size="lg" fw={700}>
+          Start Time: <Text component='span'>{order.Course.course_start_time}</Text>
+        </Text>
+        <Text size="lg" fw={700}>
+          End Time: <Text component='span'>{order.Course.course_end_time}</Text>
+        </Text>
+        <Button
+          variant="outline"
+          color="red"
+          style={{ marginTop: 20 }}
+          onClick={() => setIsModalOpen(true)}
+        >
+          Refund Order
+        </Button>
+      </Card>
+
+      <Modal
+        opened={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Confirm Refund"
+      >
+        <Text>Are you sure you want to refund this order?</Text>
+        <Group position="apart" style={{ marginTop: 20 }}>
+          <Button onClick={() => setIsModalOpen(false)}>Cancel</Button>
+          <Button color="red" onClick={handleRefund}>Confirm Refund</Button>
+        </Group>
+      </Modal>
+    </Container>
   );
 }
 
