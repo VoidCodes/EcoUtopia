9f7c1f6c95a44483665ce63fea039887d6fd65bc
VoidCodes <234273T@mymail.nyp.edu.sg>
2024-08-01 07:43:29 +0800

    more refactor

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,89 +1,53 @@
-import axios from "axios";
 import Navbar from '../components/Navbar';
 //import { useRef } from 'react';
 //import { useDisclosure } from '@mantine/hooks';
 //import { TimeInput } from '@mantine/dates';
 //import { IconClock } from '@tabler/icons-react';
-import { useState, useEffect } from "react";
-import { Button, Flex, Card, Image, Badge, Container, Group, Anchor, Text, Box, LoadingOverlay } from "@mantine/core"
+import { useEffect } from "react";
+import { Container, Box, Card, Grid, Button, Group, Anchor, Text } from "@mantine/core"
 
 function App() {
-  //const [opened, { open, close }] = useDisclosure(false);
-  const [courses, setCourses] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchCourses = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:3000/api/courses/getCourses"
-        );
-        setCourses(response.data);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-        setLoading(false);
-      }
-    };
-    fetchCourses();
-    document.title = "Courses - EcoUtopia";
+    document.title = "Home - EcoUtopia";
   });
-  if (loading) return <LoadingOverlay visible />;
-  if (error) return <Text align="center">Error: {error.message}</Text>;
-  if (courses.length === 0) return <Text align="center">No courses found</Text>;
 
   //const ref = useRef(null); 
   return (
-    <Container size={"xl"}>
+    <Container maw={800} style={{marginTop: '1rem'}}>
+      <Grid spacing="xl" display={'flex'} justifyContent={'center'} h={300}>
+        {/*<Card shadow="xl" padding="xl" radius="xl">
+          <Text align="center" size="xl" weight={700}>
+            Welcome to EcoUtopia
+          </Text>
+          <Text align="center" size="lg">
+            Your one-stop shop for all things sustainable
+          </Text>
+        </Card>*/}
+        <Grid.Col span={{ xs: 12, sm: 8, md: 6 }}>
+          <Card shadow="xl" padding="xl" radius="xl">
+            <Text align="center" size="xl" weight={700}>
+              Welcome to EcoUtopia
+            </Text>
+            <Text align="center" size="lg">
+              Your one-stop shop for all things sustainable
+            </Text>
+          </Card>
+        </Grid.Col>
+      </Grid>
       <Box padding="xl" style={{marginTop: '70px'}} />
       <Navbar />
-      {/*<Modal opened={opened} onClose={close} title="Welcome to EcoUtopia">
-        <p>This is a modal window. You can use it to display some important information to the user.</p>
-        <Group>
-          <Button color='blue' onClick={close}>Close</Button>
-        </Group>
-      </Modal>*/}
       <Group>
         <Anchor href="/test">
           <Button color='deepBlue'>Click me</Button>
         </Anchor>
-        {/*<Anchor href="/courses">
-          <Button color='red'>Click me</Button>
-        </Anchor>*/}
-        {/*<Button onClick={open} color='green'>Click me</Button> */}
-        <Anchor href="/admin/view-courses">
-          <Button color='orange'>Click me</Button>
+        <Anchor href="/testcreate">
+          <Button color='blue'>Click me</Button>
         </Anchor>
-        {/*<Anchor href="/admin/rewards">
-          <Button color='teal'>Click me</Button>
-        </Anchor> */}
+        {/*<Anchor href="/admin/view-courses">
+          <Button color='orange'>Click me</Button>
+        </Anchor>*/}
       </Group>
-      <Text 
-        align={"left"}
-        size={"xl"}
-        fw={700}
-        >Featured Courses
-      </Text>
-      <Flex>
-        {courses.map((course) => (
-          <Card key={course.course_id} shadow="xs" padding="md" style={{ width: 300, margin: 10 }}>
-            <Image src={course.course_image_url} alt={course.course_name} height={200} />
-            <Text align={"center"} size={"xl"} fw={700}>{course.course_name}</Text>
-            <Text align={"center"} size={"xl"} fw={700}>{course.course_description}</Text>
-            <Text align={"center"} size={"xl"} fw={700}>{course.price}</Text>
-            <Badge color="blue" style={{ margin: 5 }}>{course.category}</Badge>
-          </Card>
-        ))}
-      </Flex>
-      <Box padding="xl" />
-      <Text
-        align={"left"}
-        size={"xl"}
-        fw={700}
-        >Featured Rewards
-      </Text>
-      {/*<TimeInput withSeconds label="Click icon to show browser picker" ref={ref}  w={250} />*/}
     </Container>
   )
 }
